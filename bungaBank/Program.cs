cb5829eccd661a4981f78ceda56643e7e92af751
hakiki19 <mdl.hqq@gmail.com>
2020-04-06 10:32:55 +0700

    first Commit

--- bungaBank/Program.cs
+++ bungaBank/Program.cs
@@ -10,6 +10,25 @@ namespace bungaBank
     {
         static void Main(string[] args)
         {
+            double bunga = 0.00;
+            double deposit = 1000000;
+            double persentage = 1;            
+            int i = 1;
+            BungaBankUsingLoop(bunga, deposit, persentage, i);
+            Console.ReadKey();
+        }
+
+        public static void BungaBankUsingLoop(double bunga, double deposit, double persentage,int i)
+        {
+            Console.WriteLine("Hasil Dengan Looping");
+            int n = 12;
+            for (int j = 0; j < n; j++)
+            {
+                bunga = (deposit * 1) / 100;
+                deposit = deposit + bunga;
+            }
+            Console.WriteLine("Input \t " + n);
+            Console.WriteLine("Output \t " + deposit);
         }
     }
 }
